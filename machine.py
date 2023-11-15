44ccdc9ba07d02febd886238d8413bb505ff4c28
gichulLimitLess <hanascreen@konkuk.ac.kr>
2023-11-16 02:45:13 +0900

    check_valid_line 함수 2차 구현(steal 당하지 않는 최소한의 알고리즘 구현)

--- machine.py
+++ machine.py
@@ -15,38 +15,26 @@ class MACHINE():
                * Line: [(x1, y1), (x2, y2)] -> MACHINE class에서는 x값이 작은 점이 항상 왼쪽에 위치할 필요는 없음 (System이 organize 함)
     """
     
-    #유효한 선분인지 검사하는 함수 check_valid_line
+    #유효한 선분인지 검사하는 함수 check_valid_line() (+상대방에게 steal 당하지 않도록 하는 최소한의 알고리즘 적용) 
     def check_valid_line(self, line):
-        #line segment가 삼각형 안에 있거나 선분 위에 있는 경우를 탐지할 것입니다
-        def is_point_inside_triangle(p, a, b, c):
-            def sign(p1, p2, p3):
-                return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1])
-
-            d1 = sign(p, a, b)
-            d2 = sign(p, b, c)
-            d3 = sign(p, c, a)
-
-            has_neg = (d1 < 0) or (d2 < 0) or (d3 < 0)
-            has_pos = (d1 > 0) or (d2 > 0) or (d3 > 0)
-
-            return not (has_neg and has_pos)
-
-        #chain을 사용하는 대신에 sum(self.triangles,[]) 이런식으로 표현함으로서 chain과 같은 역할을 한다
-        triangle_points = list(set(sum(self.triangles, [])))
-        a, b, c = triangle_points[:2], triangle_points[2:4], triangle_points[4:]
-
-        if not (is_point_inside_triangle(line[0], a, b, c) and is_point_inside_triangle(line[1], a, b, c)):
+        #check_availability()로 1차 검사
+        #check_availability()로 선분을 그을 수 없는 예외 사항들을 모두 처리할 수 있다고 판단되었습니다. (+check_availability 약간 수정)
+        if not self.check_availability(line):
             return False
-
-        for point in self.whole_points:
-            if point in line:
-                continue
-            if is_point_inside_triangle(point, line[0], line[1], a) or \
-               is_point_inside_triangle(point, line[0], line[1], b) or \
-               is_point_inside_triangle(point, line[0], line[1], c):
+        
+        #그을려고 하는 선이 기존에 존재하는 삼각형과 함께 삼각형을 생성하는지 확인해봐야 합니다.
+        #만약 그을려고 하는 선이 기존에 존재하는 삼각형과 함께 삼각형을 생성하게 되버린다면, False를 반환해야 할 것입니다
+        new_triangle = LineString(line).intersection(LineString(self.triangle_points))
+        if not new_triangle.is_empty:
+            return False
+        
+        #선이 두 개의 기존 선에 연결되어 잠재적인 삼각형이 만들어 지는지 확인해봐야 합니다.
+        #만약 그을려고 하는 선이 기존에 존재하는 삼각형과 함께 삼각형을 생성하게 되버린다면, False를 반환해야 할 것입니다
+        connected_lines = [l for l in self.drawn_lines if set(line) & set(l)]
+        if len(connected_lines) == 2:
+            triangle = LineString(connected_lines[0] + connected_lines[1]).intersection(LineString(line))
+            if not triangle.is_empty:
                 return False
-
-        return True
     
 
     def __init__(self, score=[0, 0], drawn_lines=[], whole_lines=[], whole_points=[], location=[]):
@@ -59,8 +47,9 @@ class MACHINE():
         self.location = []
         self.triangles = [] # [(a, b), (c, d), (e, f)]
 
+    #해당 함수에서 check_availablity() 대신, check_valid_line() 사용
     def find_best_selection(self):
-        available = [[point1, point2] for (point1, point2) in list(combinations(self.whole_points, 2)) if self.check_availability([point1, point2])]
+        available = [[point1, point2] for (point1, point2) in list(combinations(self.whole_points, 2)) if self.check_valid_line([point1, point2])]
         return random.choice(available)
     
     #기본적인 line condition을 checking하는 함수입니다
@@ -89,6 +78,12 @@ class MACHINE():
 
         # Must be a new line
         condition4 = (line not in self.drawn_lines)
+
+        #삼각형의 3개의 선분 중 2개의 선분이 연결되게 되면, steal 당할 확률이 높아지겠죠. (원래 함수에서 추가 부분)
+        #그런 상황을 방지하기 위해 애초에 3개의 삼각형 선분들 중 2개의 선분이 연결되면 False(unavailable 의미) 반환
+        connected_lines = [l for l in self.drawn_lines if set(line) & set(l)]
+        if len(connected_lines) == 2:
+            return False
 
         if condition1 and condition2 and condition3 and condition4:
             return True
